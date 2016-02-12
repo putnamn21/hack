59d26672944298fdf7accdcf69d666bbfe83c698
putnamn21 <nathanputnam2@gmail.com>
2016-02-12 12:13:45 -0700

    Updated front end JS file
    added all the server posts and gets

--- public/script/script.js
+++ public/script/script.js
@@ -1,76 +1,106 @@
-var user = localStorage.getItem(JSON.parse("user"))
-if(user == undefined){
-    console.log("undefined!!!")
-   //dget(<FORM ID>).style.display = "block"; 
-}
+var user;
 
-$('#<FORM ID>').submit(function(event) {
-        var formData = {
-            'name'              : $('input[name=name]').val(),
-            'message'             : $('textarea[name=message]').val(),
-            'viewers'    : $('select[name=viewers]').val()
-        };
-        $.ajax({
-            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-            url         : '+++++++++++++++', // the url where we want to POST
-            data        : formData, // our data object
-            dataType    : 'json', // what type of data do we expect back from the server
-                        encode          : true
-        })
-            .done(function(data) {
-                saveUser(data);
-            });
-        event.preventDefault();
-        $('#<FORM ID>').css("display", "none");
-    });
+// password submission ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- --
+$("#TBD").click(function(){
+    var formData = {
+        email: $("#TBD").val(),
+        password: $("#TBD").val()
+    }
+    $.post('/userLogin', formData, function(data){
+      if(data){
+         user = data;
+      // redirect to new page if login succeeds
+      var url = "http://www.mydomain.com/new-page.html";
+      $( location ).attr("href", url);
+      } else {
+          $("#TBD").append("<p id='failedLogin' style='color:red'>Your login did not match. Please try again</p>");
+          setTimeout($('#failedLogin').remove(), 2000);
+      }   
+    }); 
+});
 
+// new user registration ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- --
 
-function saveUser(data){
-    localStorage.setItem("user", JSON.stringify(data))
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$("#TBD").click(function(){
+    //gathers user info into object
+    var formData = {
+        fName: $("#TBD").val(),
+        lName: $("#TBD").val(),
+        fullName: $("#TBD").val()+' '+$("#TBD").val(),
+        email: $("#TBD").val(),
+        password: $("#TBD").val(),
+        userId:"",  
+    }
+    //posts object to server
+    $.post('/newUser', formData, function(data){
+        if(data){
+         user = data;
+      // redirect to new page if succeeds
+      var url = "http://www.###############.com/new-page.html";
+      $(location).attr("href", url);
+      } else {
+          $("#TBD").append("<p id='failedLogin' style='color:red'>This email is already a registered user.</p>");
+          setTimeout($('#failedLogin').remove(), 2000);
+      }
+    });    
+});
 
 
+// Post question ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- --
+
+$("#TBD").click(function(){
+    //gathers user info into object
+    var formData = {
+        qId: '',
+        userId: user.userId,
+        userName: user.fullName,
+        subject: $("#TBD").val(),
+        body: $("#TBD").val(),
+        comment:[],
+        userLiked:[]
+    }
+    //posts object to server
+    $.post('/question', formData, function(data){
+        if(data){
+        //haven't built this function yet !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+         constructQuestion(data);
+      } else {
+          $("#TBD").append("<p id='failedLogin' style='color:red'>Opps, something went wrong</p>");
+          setTimeout($('#failedLogin').remove(), 2000);
+      }
+    });    
+});
 
 
+// Post comment ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
+$("#TBD").click(function(){
+    //gathers user info into object
+    var formData = {
+        qId: $(this).closest("some elemement with a certain class e.g 'div.questionHeader'").attr('id'),
+        userId: user.userId,
+        userName: user.fullName,
+        content: $(this)closest('input').val(),
+        likedCount: 0,
+        commentId:"",
+    }
+    //posts object to server
+    $.post('/comment', formData, function(data){
+        if(data){
+        //haven't built this function yet !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+        constructComment(data);
+      } else {
+          $(this).append("<p id='failedLogin' style='color:red'>Opps, something went wrong</p>");
+          setTimeout($('#failedLogin').remove(), 2000);
+      }
+    });    
+});
 
+// like a question
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function get value of html ID
-function dget (id) {
-    return(document.getElementById(id))
-}
-function submit (){
-    var a = dget("test");
-    alert(a);
-}
+$("#TBD").click(function(){
+    //posts object to server
+    $.get('/comment?id='+$(this).closest(".comment").attr("id"), function(data){
+        $(this).closest(".count").text(data);
+    });    
+});
